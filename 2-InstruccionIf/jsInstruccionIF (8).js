4e5caca0950af930ddbfbe39f0ad9a5eb71fe610
quipildor <40811766+quipildor@users.noreply.github.com>
2018-07-10 22:16:17 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
+var edad
+var estado
+
+edad = parseInt (document.getElementById("edad").value);
+estado = document.getElementById("estadoCivil").value;
+
+
+if (edad >18 && estado == "Soltero" )
+{
+    alert("Es soltero y no es menor")
+}
+
+
 
 	
 
