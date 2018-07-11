f1c3d9cbab61379d35fa6d5a7c3a8bdb52a46b73
quipildor <40811766+quipildor@users.noreply.github.com>
2018-07-10 22:17:53 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,13 @@
 
 function mostrar()
 {
+var ancho
+var largo
+var perimetro
 
+ancho = parseInt (prompt("Ingrese Ancho"));
+largo = parseInt (prompt("ingrese largo"));
+perimetro = ancho + largo + ancho + largo
+
+alert("el perimetro es " + perimetro);
 }
