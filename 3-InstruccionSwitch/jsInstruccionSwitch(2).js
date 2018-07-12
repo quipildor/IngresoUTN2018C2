e9cf9026afb8d23f93983a31767b4ed16e222ce8
quipildor <40811766+quipildor@users.noreply.github.com>
2018-07-11 22:02:47 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,9 +3,34 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
 
+switch (mesDelAño)
+{
+        case "Julio":
+        case "Agosto":
+        alert("Abrigate que hace frio");
+        break;
 
+        case "Septiembre":
+        case "Octubre":
+        case "Noviembre":
+        case "Diciembre":
+        case "Enero":
+        case "Febrero":
+        case "Marzo":
+        alert("Ya pasamos el frio, ahora calor!");
+        break;
 
+        case "Abril":
+        case "Mayo":
+        case "Junio":
+        alert("Falta para el invierno");
+        break;
+
+      
+
+     
+
+}
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -4,8 +4,16 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
+	switch (mesDelAño)
+{
+        case "Febrero":
+        alert("Este mes no tiene mas de 29 dias");
+        break;
 
+        default:
+        alert("Este mes tiene 30 o mas dias");
+
+	
+}
 
 }//FIN DE LA FUNCIÓN
