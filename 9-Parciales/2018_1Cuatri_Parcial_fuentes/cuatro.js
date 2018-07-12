274a9b94dd7fd00a96a2ab44e6919781745d2fd9
quipildor <40811766+quipildor@users.noreply.github.com>
2018-07-11 22:04:17 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,26 @@
 function mostrar()
 {
+var num1;
+var num2;
+var resultado;
 
+num1 = parseInt(prompt("Escriba un numero"));
+num2 = parseInt(prompt("Escriba otro numero"));
+
+if (num1 == num2)
+{
+    alert(num1.toString() + num2)
+}
+else if (num1 > num2) 
+{
+    alert("El resultado es " + (num1 - num2))
+}
+else
+{
+    resultado = num1 + num2
+
+    if (resultado > 10)
+    alert("La suma es " + resultado + " y supero el 10")
+    
+}
 }
