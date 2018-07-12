22fe4d7e3cf1e72a8411dc6562208fdfe07c80e1
quipildor <40811766+quipildor@users.noreply.github.com>
2018-07-11 22:14:07 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,7 +4,25 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+	switch (mesDelAño)
+    {
+        case "Febrero":
+        alert("Este mes tiene 28 dias");
+        break;
+
+        case "Septiembre":
+        case "Noviembre":
+        case "Abril":
+        case "Junio":
+        alert("Este mes tiene 30 dias");
+        break;
+        
+        default:
+        alert("Este mes tiene 31 dias");
+        
+    }
+    
+ 
 	
 
 
